59dae35840d8542a97d30f16bc13c72b0f1f64ce
marcpulz <marc_pulumbarit@hotmail.com>
2021-07-30 22:34:08 +1000

    about desc added

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,11 @@ function About() {
             
             <ParticleBackground/>
             {/* <h1>About Me</h1> */}
+            <p className="about-desc">
+                Graduating in Science In Information Technology and majoring in Interaction design at the University of Technology Sydney, enabled me to pursue my passion as a developer. Post university I continued my learning completing online courses, took on freelance projects and participated in Generation Australia's Junior Web Developer Program. Aside from coding, I love to play basketball, hit the gym, listen to podcasts and meditate. 
+                I enjoy learning, building things and creating meaningful experiences for the user.
+                Keep scrolling to check out my work!
+            </p>
         </section>
     )
 }
